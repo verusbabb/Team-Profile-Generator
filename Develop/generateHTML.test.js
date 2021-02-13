88eed52dfc73c0a902f1d230d6052796c01824d7
Steve Babb <steve.babb@outlook.com>
2021-02-13 09:24:54 -0600

    working folders and files created

---
+++ Develop/generateHTML.test.js
@@ -0,0 +1,1 @@
+const generateHTML = require('./generateHTML');
---
+++ Develop/index.js
@@ -0,0 +1,4 @@
+//Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./generateHTML');
